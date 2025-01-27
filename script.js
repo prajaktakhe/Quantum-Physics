4a707ab6f672fa7c13f2744fa11d3fc852e5e98d
prajaktakhe <khedekarprajakta136@gmail.com>
2025-01-27 15:21:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function moveRandomE1(elm) 
+{
+    elm.style.position ="absolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5 ) + "%";
+    lm.style.left = Math.floor(Math.random() * 90 + 5 ) + "%";
+}
+
+const moveRandom = document.querySelector("#move-random");
+
+moveRandom.addEventListener("mouseenter", function (e)
+{
+    moveRandomE1(e.target);
+
+}
+);
